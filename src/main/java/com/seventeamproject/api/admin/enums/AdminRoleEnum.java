033f4e62191b3639e2fd54fc72fb56fd90c601c1
hyunji <143073470+Intheji@users.noreply.github.com>
2026-02-20 14:24:32 +0900

    Feat: 관리자 Role/Status 구조 확정

--- src/main/java/com/seventeamproject/api/admin/enums/AdminRoleEnum.java
+++ src/main/java/com/seventeamproject/api/admin/enums/AdminRoleEnum.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum AdminRoleEnum {
 
     SUPER_ADMIN("ROLE_SUPER_ADMIN", "슈퍼 관리자"),
-    ADMIN("ROLE_ADMIN", "관리자");
+    OPERATION_ADMIN("ROLE_OPERATION_ADMIN","운영 관리자"),
+    CS_ADMIN("ROLE_CS_ADMIN", "CS 관리자");
 
     private final String authority;
     private final String title;
