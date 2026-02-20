15d9a2ba2e4c306394d852f12ec52a8469fca23c
hoon3948 <robot3949@gmail.com>
2026-02-20 17:57:54 +0900

    feat:고객 3레이어,엔티티,dto 작성

---
+++ src/main/java/com/seventeamproject/api/customer/repository/CustomerRespositoryCustom.java
@@ -0,0 +1,9 @@
+package com.seventeamproject.api.customer.repository;
+
+import com.seventeamproject.api.customer.dto.CustomersResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface CustomerRespositoryCustom {
+//    Page<CustomersResponse> search(Pageable pageable, String title);
+}
