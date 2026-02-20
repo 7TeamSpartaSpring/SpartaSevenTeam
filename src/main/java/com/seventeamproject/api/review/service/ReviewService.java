9a2e9f7954f1dacc910a55510da48bcf95b96f82
Taeguni06 <aqwsde0412@gmail.com>
2026-02-20 20:29:27 +0900

    feat: 리뷰 관련 기능 업데이트 / 리뷰 데이터 자동 생성 / 리뷰 엔티티 업데이트 (#38)
    * feat: 리뷰 데이터 자동 생성 / 리뷰 엔티티 업데이트
    
    * feat: 리뷰 관련 기능 업데이트

--- src/main/java/com/seventeamproject/api/review/service/ReviewService.java
+++ src/main/java/com/seventeamproject/api/review/service/ReviewService.java
@@ -1,13 +1,21 @@
 package com.seventeamproject.api.review.service;
 
+import com.seventeamproject.api.review.dto.ReviewResponse;
 import com.seventeamproject.api.review.repository.ReviewRepository;
+import com.seventeamproject.common.dto.PageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class ReviewService {
     private final ReviewRepository reviewRepository;
 
-
+    public PageResponse<ReviewResponse> getAll(Pageable pageable, String keyword, Integer rating) {
+        return new PageResponse<>(reviewRepository.search(pageable, keyword, rating));
+    }
 }
+
