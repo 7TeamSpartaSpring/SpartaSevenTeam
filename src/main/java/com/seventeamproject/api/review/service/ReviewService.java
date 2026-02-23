fca6caaadec89c03ff96bda39716bfd903bcde24
Taeguni06 <aqwsde0412@gmail.com>
2026-02-23 10:32:26 +0900

    feat: Review Crud 구현 (#48)

--- src/main/java/com/seventeamproject/api/review/service/ReviewService.java
+++ src/main/java/com/seventeamproject/api/review/service/ReviewService.java
@@ -1,6 +1,7 @@
 package com.seventeamproject.api.review.service;
 
 import com.seventeamproject.api.review.dto.ReviewResponse;
+import com.seventeamproject.api.review.dto.ReviewUpdateRequest;
 import com.seventeamproject.api.review.repository.ReviewRepository;
 import com.seventeamproject.common.dto.PageResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +15,21 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReviewService {
     private final ReviewRepository reviewRepository;
 
+    @Transactional(readOnly = true)
     public PageResponse<ReviewResponse> getAll(Pageable pageable, String keyword, Integer rating) {
         return new PageResponse<>(reviewRepository.search(pageable, keyword, rating));
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewResponse getOne(Long id) {
+        return new ReviewResponse(reviewRepository.findById(id).orElseThrow(
+                () -> new IllegalStateException("존재하지 않는 리뷰입니다.")
+        ));
+    }
+
+    public ReviewResponse update(Long id, ReviewUpdateRequest request) {
+        return new ReviewResponse(reviewRepository.findById(id).
+                orElseThrow(()-> new IllegalStateException("존재하지 않는 리뷰입니다.")).update(request.content()));
     }
 }
 
