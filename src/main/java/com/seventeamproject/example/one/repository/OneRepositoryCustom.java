8424b7b03b6d9d3cda4ced341de83ebc5b47bbf6
Taeguni06 <aqwsde0412@gmail.com>
2026-02-25 19:52:07 +0900

    chore: example 폴더 삭제

--- src/main/java/com/seventeamproject/example/one/repository/OneRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.seventeamproject.example.one.repository;
-
-import com.seventeamproject.example.one.dto.OnesResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface OneRepositoryCustom {
-    Page<OnesResponse> search(Pageable pageable, String title);
-}
